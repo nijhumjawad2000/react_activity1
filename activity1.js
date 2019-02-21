aafdd0f236e2124e2fda9df13605aa1b0e9a3e10
nijhum_jawad2000 <njawad@techlaunch.io>
2019-02-22 05:36:00 +0600

    first commit

---
+++ activity1.js
@@ -0,0 +1,28 @@
+class Developer{
+    constructor(firstname, lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    getName(){
+        return this.firstname + " " +this.lastname;
+    }
+}
+var developer1 = new Developer('Nijhum', 'Jawad');
+
+console.log(developer1.getName());
+
+class JavaScriptDeveloper extends Developer{
+    getJob(){
+        return 'JavaScript Developer';
+    }
+}
+
+var developer1 = new JavaScriptDeveloper('Zakia', 'Jeme');
+
+console.log(developer1.getName());
+console.log(developer1.getJob());
+
+const Greeting = () => {
+    return 'Welcome to React Dev';
+}
+console.log(Greeting());
